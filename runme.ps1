4c3ac9af40ea850d48821b5793fbeefc01b0adac
Ulic Belouve <ulic.belouve@gmail.com>
2017-01-13 15:31:01 -0600

    Update runme.ps1

--- runme.ps1
+++ runme.ps1
@@ -1,18 +1,21 @@
+Write-Host " My execution policy was: "
 Get-ExecutionPolicy
+Start-Sleep 3
 Write-Host " My voice is my passport, verify me "
 Write-Host "  LOL U LOSE"
-Write-Host " What, I was set to restricted? "
+Write-Host " What, I exceeded restrictions? "
 Write-Host "   LOL U LOSE"
 Write-Host " My voice is my passport, verify me "
 Write-Host "    LOL U LOSE"
+Start-Sleep 3
 Write-Host " My voice is my passport, verify me "
 Write-Host "     LOL U LOSE"
-Write-Host " My voice is my passport, verify me "
-Write-Host "      LOL U LOSE"
 Write-Host " I'm in your base, stealin' your flag "
-Write-Host "       LOL U LOSE"
-Write-Host " My voice is my passport, verify me "
 Write-Host "      LOL U LOSE"
+Write-Host " My voice is my passport, verify me "
+Write-Host "       LOL U LOSE"
+Start-Sleep 2
 Write-Host " (Attempt to clear Security and Application Event Log) "
+Start-Sleep 1
 Get-EventLog -list
 Clear-EventLog -logname Application, Security -computername $env:COMPUTERNAME
