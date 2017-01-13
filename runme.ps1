d7d9e619c0162307516671d2a5d95c33ad303092
Ulic Belouve <ulic.belouve@gmail.com>
2017-01-13 16:01:17 -0600

    Update runme.ps1
    Now with 100% more Bob Barker

--- runme.ps1
+++ runme.ps1
@@ -3,19 +3,27 @@ Get-ExecutionPolicy
 Start-Sleep 3
 Write-Host " My voice is my passport, verify me "
 Write-Host "  LOL U LOSE"
+Start-Sleep 1
 Write-Host " What, I exceeded restrictions? "
 Write-Host "   LOL U LOSE"
+Start-Sleep 1
 Write-Host " My voice is my passport, verify me "
 Write-Host "    LOL U LOSE"
-Start-Sleep 3
+Start-Sleep 1
 Write-Host " My voice is my passport, verify me "
 Write-Host "     LOL U LOSE"
+Start-Sleep 1
 Write-Host " I'm in your base, stealin' your flag "
 Write-Host "      LOL U LOSE"
+Start-Sleep 1
 Write-Host " My voice is my passport, verify me "
 Write-Host "       LOL U LOSE"
-Start-Sleep 2
-Write-Host " (Attempt to clear Security and Application Event Log) "
 Start-Sleep 1
-Get-EventLog -list
-Clear-EventLog -logname Application, Security -computername $env:COMPUTERNAME
+Write-Host " Wait for it... "
+Start-Sleep 1
+Start http://www.priceisrightfailhorn.com/
+# Remove comment octothorpe from below lines to attempt Log Clearing
+# Write-Host " (Attempt to clear Security and Application Event Log) "
+# Start-Sleep 1
+# Get-EventLog -list
+# Clear-EventLog -logname Application, Security -computername $env:COMPUTERNAME
