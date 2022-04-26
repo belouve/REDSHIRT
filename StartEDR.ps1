0540d188a115efc6f69894e1b497e0eba2022040
Ulic Belouve <ulic.belouve@gmail.com>
2022-04-26 14:11:56 -0500

    Initial upload, RTFC.
    To help start a service based on a list. Read comments in the code.

---
+++ StartEDR.ps1
@@ -0,0 +1,15 @@
+﻿$targets = Get-Content -Path C:\Projects\PowershellEDR\edr-target-list1.txt
+ForEach ($target in $targets) {
+  If((Get-Service -Name carbonblack -ComputerName $target -ErrorAction SilentlyContinue).Status -eq 'Stopped'){
+    #The above makes it only apply if Stopped, and to silently continue on error
+	#Then sets the service to Running
+	Get-Service -Name carbonblack -ComputerName $target | Set-Service -Status Running
+    #Let sleep to ensure started
+        Start-sleep -Seconds 2
+	#This is to give us some feedback, see when new one started
+    Write-Host "Started EDR, status is:"	
+    Get-Service -Name carbonblack -ComputerName $target
+  }
+}
+#And to close it out to let us know
+Write-Host "All done with list!"
